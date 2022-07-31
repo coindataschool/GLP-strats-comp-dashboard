a69dcdefa9076dde6648b7457e11eda0707ad7d5
Coin Data School <coindataschool@gmail.com>
2022-07-31 15:08:27 +0900

    add contact info

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ st.sidebar.title('Choose Chain')
 selection = st.sidebar.radio("", ["Arbitrum", "Avalanche"])
 with st.sidebar:
     st.write("Subscribe to my free [newsletter](https://coindataschool.substack.com/about) to get data-driven insights about DeFi and NFT projects.")
+    st.write("Follow me on twitter: [@coindataschool](https://twitter.com/coindataschool)")
+    st.write("Buy me a coffee: 0x783c5546c863f65481bd05fd0e3fd5f26724604e")
 
 # main section
 st.header('Backtest and Compare GLP Strategies')
