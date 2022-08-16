43797c8f9ef8e1cbc9a89f21b35b55ca14a2514e
Coin Data School <coindataschool@gmail.com>
2022-08-16 15:14:40 +0900

    add tippin me

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ with st.sidebar:
     st.write("Subscribe to my free [newsletter](https://coindataschool.substack.com/about) to get data-driven insights about DeFi and NFT projects.")
     st.write("Check out my other dashboards on Dune: [@coindataschool](https://dune.com/coindataschool)")
     st.write("Follow me on twitter: [@coindataschool](https://twitter.com/coindataschool)")
-    st.write("Buy me a coffee: 0x783c5546c863f65481bd05fd0e3fd5f26724604e")
+    st.markdown("Buy me a coffee with eth coins: `0x783c5546c863f65481bd05fd0e3fd5f26724604e`")
+    st.markdown("[Tip me sat](https://tippin.me/@coindataschool)")
 
 # main section
 st.header('Backtest and Compare GLP Strategies')
