6e90438bbe31cc8f948c2839742f2bdfa9feca92
Coin Data School <coindataschool@gmail.com>
2022-09-12 10:35:26 +0900

    format sidebar text

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ st.set_page_config(page_title='Backtest GLP Strategies', layout='wide', page_ico
 st.sidebar.title('Choose Chain')
 selection = st.sidebar.radio("", ["Arbitrum", "Avalanche"])
 with st.sidebar:
-    st.markdown("## Get insights and Learn DeFi analytics:")
+    st.markdown("## Get Insights")
     st.markdown("- Subscribe to my [newsletter](https://coindataschool.substack.com/about)")
     st.markdown("- Follow me on twitter: [@coindataschool](https://twitter.com/coindataschool)")
     st.markdown("- Follow me on github: [@coindataschool](https://github.com/coindataschool)")
-    st.markdown("## Try GMX:")
+    st.markdown("## Try GMX")
     st.markdown("- Get [5% discount](https://gmx.io/#/?ref=coin_data_school) when trading on GMX.")
-    st.markdown("## Buy me a coffee:")
-    st.markdown("ETH Wallet: `0x783c5546c863f65481bd05fd0e3fd5f26724604e`")
-    st.markdown("[Tip me sat](https://tippin.me/@coindataschool)")
+    st.markdown("## Support My Work")
+    st.markdown("- ETH Wallet: `0x783c5546c863f65481bd05fd0e3fd5f26724604e`")
+    st.markdown("- [Tip me sat](https://tippin.me/@coindataschool)")
 
 # main section
 st.header('Backtest and Compare GLP Strategies')
