21eeeefb7d026793fa8dd1a8fb190d7ffe67b61b
Coin Data School <coindataschool@gmail.com>
2022-09-12 10:19:39 +0900

    add gmx referrals

--- main.py
+++ main.py
@@ -10,11 +10,12 @@ st.sidebar.title('Choose Chain')
 selection = st.sidebar.radio("", ["Arbitrum", "Avalanche"])
 with st.sidebar:
     st.write("Subscribe to my free [newsletter](https://coindataschool.substack.com/about) to get data-driven insights about DeFi and NFT projects.")
-    st.write("Check out my other dashboards on Dune: [@coindataschool](https://dune.com/coindataschool)")
     st.write("Follow me on twitter: [@coindataschool](https://twitter.com/coindataschool)")
+    st.markdown("Get [5% discount](https://gmx.io/#/?ref=coin_data_school) when trading on GMX.")
     st.markdown("Buy me a coffee with eth coins: `0x783c5546c863f65481bd05fd0e3fd5f26724604e`")
     st.markdown("[Tip me sat](https://tippin.me/@coindataschool)")
-
+    st.write("Check out my Dune dashboards: [@coindataschool](https://dune.com/coindataschool)")
+    
 # main section
 st.header('Backtest and Compare GLP Strategies')
 backtest.run_app(chain = selection)
